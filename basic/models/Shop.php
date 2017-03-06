8348328734391c9c68592db07bfa987d52cb2d39
freeman306 <997987618@qq.com>
2017-03-06 09:48:57 +0800

    yiishiop

---
+++ basic/models/Shop.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Shop extends ActiveRecord{
+    public static function tableName(){
+        return "{{%shop}}";
+    }
+}
---
+++ basic/modules/controllers/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\modules\controllers;
+use yii\web\Controller;
+
+class DefaultController extends Controller{
+    public function actionIndex(){
+        $this->layout = 'layout1';
+        return  $this->render('index');
+    }
+}
